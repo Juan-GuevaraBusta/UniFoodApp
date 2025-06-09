ab19a6674f738c0466ec55d036b3b692d6cab278
Juan-GuevaraBusta <jguevarabustamante25@gmail.com>
2025-06-09 18:07:09 -0500

    NativeWind and Tailwind Configuration were complete

--- Desktop/universidad/NeoDigital/UniFoodApp/app/_layout.tsx
+++ Desktop/universidad/NeoDigital/UniFoodApp/app/_layout.tsx
@@ -1,9 +1,9 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, ThemeProvider, } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
+import "@/global.css";
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 
@@ -20,7 +20,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
