f2b4bc8cf25ef8ab44a706a73764b49055bb0b5a
Juan-GuevaraBusta <jguevarabustamante25@gmail.com>
2025-06-09 18:33:28 -0500

    ESLint and prettier added to help with understanding code

--- Desktop/universidad/NeoDigital/UniFoodApp/app/(tabs)/index.tsx
+++ Desktop/universidad/NeoDigital/UniFoodApp/app/(tabs)/index.tsx
@@ -1,13 +1,12 @@
 // This file shows the initial page of the app
-import { Platform, StyleSheet, Text, StatusBar } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { Text, StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView className ="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
       <Text className={"text-red-500"}>¡Bienvenido a UniFood!</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
-  )  
+  );
 }
